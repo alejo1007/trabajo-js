784bfe1fc303f00e43a399becfaf35800811d6fc
alejo1007 <68398182+alejo1007@users.noreply.github.com>
2022-03-03 18:46:27 -0300

    pedir datos user para ficha

--- script.js
+++ script.js
@@ -172,5 +172,57 @@ function saludar() {
 
   return cad;
 }
+class Persona {
+  constructor(nombre, apellido, edad) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+  }
 
-document.write(saludar());
+  hablar() {
+    console.log("");
+  }
+}
+
+let personas = [];
+
+if (localStorage.getItem("Personas")) {
+  personas = JSON.parse(localStorage.getItem("Personas"));
+} else {
+  localStorage.setItem("Personas", JSON.stringify(personas));
+}
+
+let form = document.getElementById("formUser");
+let boton = document.getElementById("botonUsers");
+let div = document.getElementById("divUsers");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let nombre = document.getElementById("name").value;
+  let apellido = document.getElementById("apellido").value;
+  let edad = document.getElementById("edad").value;
+
+  const persona = new Persona(nombre, apellido, edad);
+  personas.push(persona);
+
+  localStorage.setItem("Personas", JSON.stringify(personas));
+  form.reset();
+});
+
+boton.addEventListener("click", () => {
+  let arrayStorage = JSON.parse(localStorage.getItem("Personas"));
+  div.innerHTML = "";
+
+  arrayStorage.forEach((personaEnArray, indice) => {
+    div.innerHTML += `
+          <div class="card" id="persona${indice}" style="width: 18rem;">
+              <div class="card-body">
+                  <h5 class="card-title">${personaEnArray.nombre}</h5>
+                  <p class="card-text">${personaEnArray.apellido}</p>
+                  <p class="card-text">${personaEnArray.edad}</p>
+              </div>
+          </div>
+      `;
+  });
+});
