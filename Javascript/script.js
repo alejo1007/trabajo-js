57102183ff23ab22aa996a20332c836a53fc20ee
alejo1007 <68398182+alejo1007@users.noreply.github.com>
2022-04-14 17:57:50 -0300

    correcciones

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,3 @@
-swal("Aprieta el boton para continuar").then((value) => {
-  swal(`Muchas gracias.: ${value}`);
-});
-
 //constructor
 function Seguro(marca, anio, tipo) {
   this.marca = marca;
@@ -65,7 +61,7 @@ Interfaz.prototype.mostarMensaje = function (mensaje, tipo) {
 
   setTimeout(function () {
     document.querySelector(".mensaje").remove();
-  }, 2000);
+  }, 2022);
 };
 
 //imprime resultado de cotización
@@ -75,13 +71,13 @@ Interfaz.prototype.mostrarResultado = function (seguro, total) {
 
   switch (seguro.marca) {
     case "1":
-      marca = "Americano";
+      marca = "Volkswagen";
       break;
     case "2":
-      marca = "Asiático";
+      marca = "Honda";
       break;
     case "3":
-      marca = "Europeo";
+      marca = "Audi";
       break;
   }
 
@@ -154,125 +150,3 @@ for (let i = max; i > min; i--) {
   option.innerHTML = i;
   selectAnios.appendChild(option);
 }
-
-/*Saludo segun hora*/
-
-class User {
-  constructor(nombre, email, password) {
-    this.nombre = nombre;
-    this.email = email;
-    this.password = password;
-  }
-}
-
-let usuarios = [];
-
-if (localStorage.getItem("Users")) {
-  usuarios = JSON.parse(localStorage.getItem("Users"));
-} else {
-  localStorage.setItem("Users", JSON.stringify(usuarios));
-}
-
-let formUsers = document.getElementById("formUser");
-let botonUsers = document.getElementById("botonUsers");
-let divUsers = document.getElementById("divUsers");
-
-formUsers.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let nombre = document.getElementById("usernameID").value;
-  let email = document.getElementById("emailID").value;
-  let password = document.getElementById("passwordID").value;
-  const user = new User(nombre, email, password);
-  usuarios.push(user);
-
-  localStorage.setItem("Users", JSON.stringify(usuarios));
-  formUsers.reset();
-});
-
-botonUsers.addEventListener("click", () => {
-  divUsers.innerHTML = "";
-
-  usuarios.forEach((usuariosEnArray, indice, array) => {
-    console.log(array);
-
-    divUsers.innerHTML += `
-          <div id="usuario${indice}" class="card border-secondary mb-3" style="max-width: 20rem;margin:10px">
-              <div class="card-header"><h4>Usuario ${usuariosEnArray.nombre}</h4></div>
-                  <div class="card-body">
-                      <p class="card-title">Email: ${usuariosEnArray.email}</p>
-                      <button id="boton${indice}" class="btn btn-danger">Eliminar</button>
-                  </div>
-              </div>
-      `;
-  });
-  //usuario.nombreAtributo = nuevoAtributo
-  usuarios.forEach((usuariosEnArray, indice) => {
-    document.getElementById(`boton${indice}`).addEventListener("click", () => {
-      divUsers.removeChild(document.getElementById(`usuario${indice}`));
-      let indiceArray = usuarios.findIndex(
-        (user) => user.nombre == usuariosEnArray.nombre
-      );
-      usuarios.splice(indiceArray, 1);
-      localStorage.setItem("Users", JSON.stringify(usuarios));
-    });
-  });
-});
-
-/*-- funcion nadbar--*/
-$(document).ready(main);
-
-var contador = 1;
-
-function main() {
-  $(".menu_bar").click(function () {
-    // $('nav').toggle();
-
-    if (contador == 1) {
-      $("nav").animate({
-        left: "0",
-      });
-      contador = 0;
-    } else {
-      contador = 1;
-      $("nav").animate({
-        left: "-100%",
-      });
-    }
-  });
-}
-/*-- fin nadbar--*/
-
-/*--comienza carusell--*/
-var myCarousel = document.querySelector("#myCarousel");
-var carousel = new bootstrap.Carousel(myCarousel);
-var myCarousel = document.querySelector("#myCarousel");
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000,
-  wrap: false,
-});
-
-/*----- */
-let divDolar = document.getElementById("divDolar");
-
-document.getElementById("botonDolar").addEventListener("click", () => {
-  fetch("https://criptoya.com/api/dolar")
-    .then((promesa) => promesa.json())
-    .then((data) => {
-      let { oficial, blue, ccb, ccl, mep, solidario } = data;
-      divDolar.innerHTML = `
-            <p>Oficial:$ ${oficial} </p>
-            <p>Solidario:$ ${solidario} </p>
-            <p>Blue:$ ${blue} </p>
-            <p>Contado con Bitcoin:$ ${ccb}</p>
-            <p>Contado con liquidacion:$ ${ccl} </p>
-            <p>Mep: $${mep} </p>
-           
-        `;
-    });
-});
-
-document.getElementById("botonClientes").addEventListener("click", () => {
-  fetch("./json/clientes.json")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-});
